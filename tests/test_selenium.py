98237847d27d02ad4dc765204c01fcb27472d2cd
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-16 15:03:37 +0300

    testing

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 import pytest
 
-
+@pytest.mark.my_selenium
 def test_selenium():
     driver = webdriver.Chrome()
     driver.get("https://www.selenium.dev/")
-    assert driver.title == "selenium"
+    assert driver.title == "Selenium"
     driver.quit()
